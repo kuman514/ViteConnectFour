8922d74c76ae445e823f076f9a36953de36927aa
kuman514 <hadjadj0@gmail.com>
2025-07-07 03:01:32 +0900

    refactor: move app module

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import PageTemplate from '^/shared/page-template';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PageTemplate />}>
+          {/* <Route index element={<GamePage />} />
+          <Route path="replay" element={<ReplayPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
